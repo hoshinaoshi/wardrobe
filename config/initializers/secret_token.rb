fbc498d32bf0830f7f7ca337c6b39b7260d6765b
Naoshi Hoshi <hoshinaoshi@gmail.com>
2014-01-26 00:10:42 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wardrobe::Application.config.secret_key_base = '66de7d912a82b8abc13bcd163ef7179f0800092ff7345e6bbb3ac639cfd73c06cde5488f09b5442c9f98767ced77f7a56a1c02c52bde6ff4aa2dab3961a38215'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Wardrobe::Application.routes.draw do
+  get "top/index"
+  root to: 'top#index'
+end
